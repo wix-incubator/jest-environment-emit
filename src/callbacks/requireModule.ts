5788cfb2ded0a951f70ba93de93dad780b4e4342
Yaroslav Serhieiev <yaroslavs@wix.com>
2023-11-26 11:17:14 +0200

    fix: module import

--- src/callbacks/requireModule.ts
+++ src/callbacks/requireModule.ts
@@ -8,10 +8,18 @@ export async function requireModule<E extends JestEnvironment = JestEnvironment>
 ): Promise<EnvironmentListenerOnly<E> | null> {
   try {
     const cwdPath = require.resolve(moduleName, { paths: [rootDir] });
-    const result = (await import(cwdPath)) as any;
-    return (result?.default ?? result) as EnvironmentListenerOnly<E>;
+    return (await importModule(cwdPath)) as EnvironmentListenerOnly<E>;
   } catch (error: any) {
     logger.warn({ cat: 'import', err: error }, `Failed to resolve: ${moduleName}`);
     return null;
   }
+}
+
+async function importModule(absolutePath: string): Promise<any> {
+  let result = await import(absolutePath);
+  if (result.__esModule) {
+    result = result.default;
+  }
+
+  return result.default ?? result;
 }
